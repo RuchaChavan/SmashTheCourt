bca7308c6f9ecbea78f5048d4730462f8be07150
Gaurav <gauravsanjaybhavsar@gmail.com>
2022-02-25 23:47:18 +0530

    MVC Home Page , Login Page And Registration Page .metadata/.plugins/org.eclipse.core.resources/.history/12/b069c0d02596001c1497ceed069a01e2# new file: .metadata/.plugins/org.eclipse.core.resources/.history/14/1004e3a72296001c1497ceed069a01e2 .metadata/.plugins/org.eclipse.core.resources/.history/14/701816371496001c1497ceed069a01e2

---
+++ SmashTheCourt/src/main/java/com/project/SmashTheCourtApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashTheCourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashTheCourtApplication.class, args);
+	}
+
+}
